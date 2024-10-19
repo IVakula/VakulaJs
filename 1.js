bad4431ce87df29ce1adb9940b6334b3e86bf307
IrynaVakula <vakulaiv@gmail.com>
2024-10-19 18:22:28 +0200

    added homework7_1

---
+++ 1.js
@@ -0,0 +1,21 @@
+git let getInfo = Symbol('getInfo');
+let object = {
+    name: 'Panda',
+    kindom: 'Animals',
+    size: 'up to 1.8m',
+    weight: 'up to 160kg',
+    [getInfo]() {
+        console.log('Our object:');
+        for (let key in this) {
+            console.log(key + ': ' + this[key]);
+        }
+        console.log();
+    }
+}
+object[getInfo]();
+
+object.areaOfHabitat = 'China';
+object.nutrition = 'omnivores';
+
+object[getInfo]();
+
